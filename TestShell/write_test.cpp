90637130d00ba3ee7e8a6296dbf787bd1fb5ec2e
SonMinki <m21.son@samsung.com>
2025-05-16 16:45:56 +0900

    [TEST] write unit Test 내 vector parameter 적용
    write unit test 진행시 vector parameter 사용하도록 fixture 구성

--- TestShell/write_test.cpp
+++ TestShell/write_test.cpp
@@ -1,20 +1,28 @@
 #include "gmock/gmock.h"
 #include "MockDriver.h"
 #include "TestShell.h"
+#include <vector>
 using namespace testing;
 
 class WriteFixture : public Test {
 public:
 	MockDriver mockdriver;
 	TestShell testshell{ &mockdriver };
+	const unsigned int NORMAL_ADDRESS = 3;
+	const unsigned int NORMAL_DATA = 0xAAAABBBB;
+	const unsigned int RANGE_OVER_ADDRESS = 100;
+	const unsigned int RANGE_UNDER_ADDRESS = -1;
+	std::vector<unsigned int> normal_param = { NORMAL_ADDRESS,NORMAL_DATA };
+	std::vector<unsigned int> range_over_param = { RANGE_OVER_ADDRESS,NORMAL_DATA };
+	std::vector<unsigned int> range_under_param = { RANGE_OVER_ADDRESS,NORMAL_DATA };
 };
 
 TEST_F(WriteFixture, NormalWritePass) {
-	EXPECT_CALL(mockdriver, write(3, 0xAAAABBBB)).Times(1);
-	testshell.write(3, 0xAAAABBBB);
+	EXPECT_CALL(mockdriver, write(NORMAL_ADDRESS, NORMAL_DATA)).Times(1);
+	testshell.write(normal_param);
 }
-TEST_F(WriteFixture, AddressRangeOverFail) {
+TEST_F(WriteFixture, AddressRangeFail) {
 	EXPECT_CALL(mockdriver, write(_,_)).Times(0);
-	EXPECT_THROW(testshell.write(testshell.ADDRESS_RANGE_MAX + 1, 0xAAAABBBB), std::exception);
-	EXPECT_THROW(testshell.write(testshell.ADDRESS_RANGE_MIN - 1, 0xAAAABBBB), std::exception);
+	EXPECT_THROW(testshell.write(range_over_param), std::exception);
+	EXPECT_THROW(testshell.write(range_under_param), std::exception);
 }
