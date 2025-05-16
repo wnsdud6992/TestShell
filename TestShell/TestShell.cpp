88378fff286cf4af5d4a1cd2025776fe10e20198
SonMinki <m21.son@samsung.com>
2025-05-16 15:26:46 +0900

    [TEST] : Write Command Unit Test 추가, Address range over
    과제 자료에 0~99 LBA가 spec이므로, 99가 over 되는 경우에 대한 unit test 작성

--- TestShell/TestShell.cpp
+++ TestShell/TestShell.cpp
@@ -2,6 +2,8 @@
 
 TestShell::TestShell(IDriver* driver_) : driver(driver_) {}
 void TestShell::write(int address, int data) {
+	if (address < 0 || address > 99)
+		return;
 	driver->write(address, data);
 }
 
--- TestShell/write_test.cpp
+++ TestShell/write_test.cpp
@@ -9,3 +9,9 @@ TEST(WriteTest, NormalWritePass) {
 	TestShell testshell{ &mockdriver };
 	testshell.write(3, 0xAAAABBBB);
 }
+TEST(WriteTest, AddressRangeOverFail) {
+	MockDriver mockdriver;
+	EXPECT_CALL(mockdriver, write(_,_)).Times(0);
+	TestShell testshell{ &mockdriver };
+	testshell.write(100, 0xAAAABBBB);
+}
