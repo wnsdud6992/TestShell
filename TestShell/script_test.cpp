a0b64db8e7ae834012031a5a010f8970f8af8247
jnyoung.choi <wnsdud6992@naver.com>
2025-05-19 13:14:07 +0900

    [REFACT] ReadCompare method

--- TestShell/script_test.cpp
+++ TestShell/script_test.cpp
@@ -3,12 +3,15 @@
 #include "TestShell.h"
 using namespace testing;
 
-TEST(ScriptTest, ReadCompareTest) {
+class ScriptTestFixture : public Test {
+public:
 	MockDriver mockdriver;
 	TestShell testshell{ &mockdriver };
-	std::vector<unsigned int> address = {20};
-	unsigned int value = 0x1289CDEF;
-	EXPECT_CALL(mockdriver, read(address[0])).WillOnce(Return(value));
+	std::vector<unsigned int> TEST_ADDRESS = { 20 };
+	const unsigned int TEST_VALUE = 0x1289CDEF;
+};
 
-	EXPECT_TRUE(testshell.readCompare(address, value));
+TEST_F(ScriptTestFixture, ReadCompareTest) {
+	EXPECT_CALL(mockdriver, read(TEST_ADDRESS[0])).WillOnce(Return(TEST_VALUE));
+	EXPECT_TRUE(testshell.readCompare(TEST_ADDRESS, TEST_VALUE));
 }
