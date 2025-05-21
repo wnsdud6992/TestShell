2d06e2d6f784826838d3baa2c228dd6b04d844ca
SonMinki <smg2252@gmail.com>
2025-05-21 10:19:34 +0900

    [REFAC] SSDdriver 메소드 정리
     중복 코드 정리 함수 추출 과 overloading 활용

--- TestShell/SSDDriver.cpp
+++ TestShell/SSDDriver.cpp
@@ -1,31 +1,58 @@
 ﻿#include "SSDDriver.h"
 
-void SSDDriver::write(unsigned int address, unsigned int value) {
-	std::string SSD_exe_Writeparam = "";
+void SSDDriver::runFunc(const std::string option, unsigned int param1 = 0, unsigned int param2 = 0)
+{
+	const unsigned int address = param1;
+	const unsigned int address2 = param2;
+	const unsigned int value = param2;
 	std::ostringstream oss;
-	oss << "W " << address << " " << toHexString(value);
-	SSD_exe_Writeparam += oss.str();
-	runSSDWithParam(SSD_exe_Writeparam);
-}
-unsigned int SSDDriver::read(unsigned int address) {
-	std::string SSD_exe_Readparam = "";
-	std::ostringstream oss;
-	oss << "R " << address;
-	SSD_exe_Readparam += oss.str();
-	runSSDWithParam(SSD_exe_Readparam);
+	oss << option << " ";
+	if (option == "W") {
+		oss << address << " ";
+		oss << toHexString(value);
+	}
+	else if (option == "R") {
+		oss << address << " ";
+	}
+	else if (option == "E") {
+		oss << address << " ";
+		oss << address2;
 
+	}
+	else if (option == "F") {
+		; //nothing
+	}
+	else {
+		throw CustomException("undefined command for call SSD.exe run");
+	}
+
+	std::string command = "\"" + SSDexe_Path + "\" " + oss.str();
+	std::cout << "Command: " << command << std::endl;  // Todo. 추후 제거
+	int result = std::system(command.c_str());
+}
+
+void SSDDriver::write(unsigned int address, unsigned int value) {
+	runFunc("W", address, value);
+}
+
+unsigned int SSDDriver::read(unsigned int address) {
+	runFunc("R", address);
 	return readSSDOutputFile();
+}
+
+void SSDDriver::erase(unsigned int address, unsigned int size) {
+	if (size > MAX_ERASE_SIZE || size < 0) {
+		throw CustomException("Erase size range error");
+	}
+	runFunc("E", address, size);
+}
+
+void SSDDriver::flush() {
+	runFunc("F");
 }
 
 void SSDDriver::setoutput(std::ostream* output){
 	this->out = output;
-}
-
-void SSDDriver::runSSDWithParam(const std::string& param) {
-	std::string command = "\"" + SSDexe_Path + "\" " + param;
-	std::cout << "Command: " << command << std::endl;  // Todo. 추후 제거
-	int result = std::system(command.c_str());
-	// std::cout << "Exit code: " << result << std::endl;
 }
 
 unsigned int SSDDriver::readSSDOutputFile() {
@@ -44,23 +71,17 @@ unsigned int SSDDriver::readSSDOutputFile() {
 	}
 
 	inputFile.close();
-	return std::stoul(readvalue, nullptr, 0);
-}
-void SSDDriver::erase(unsigned int address, unsigned int size) {
-	std::string SSD_exe_Eraseparam = "";
-	std::ostringstream oss;
-	oss << "E " << address << " " << size;
-	if (size > MAX_ERASE_SIZE || size < 0) {
-		throw CustomException("Erase size range error");
-	}
-	SSD_exe_Eraseparam += oss.str();
-	runSSDWithParam(SSD_exe_Eraseparam);
-}
-void SSDDriver::flush() {
-	std::string SSD_exe_Flushparam = "";
-	std::ostringstream oss;
-	oss << "F ";
-	SSD_exe_Flushparam += oss.str();
-	runSSDWithParam(SSD_exe_Flushparam);
 
+	unsigned int result_readvalue = 0;
+	try {
+		result_readvalue = std::stoul(readvalue, nullptr, 0);
+	}
+	catch (const std::invalid_argument&) {
+		std::cerr << "[Error] 숫자로 변환할 수 없는 문자열: " << readvalue << std::endl;
+	}
+	catch (const std::out_of_range&) {
+		std::cerr << "[Error] 숫자가 너무 큽니다: " << readvalue << std::endl;
+	}
+
+	return result_readvalue;
 }
